58b18ecc4d446409c5250295f8e5689913808e30
M. Mohan <20501a05c1@pvpsit.ac.in>
2024-02-23 14:14:17 +0530

    Connected Database

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-import app from "./app.mjs";
 import dotenv from "dotenv";
+
+import app from "./app.mjs";
+import pool from "./config/db.js";
 
 dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT || 3000;
 
+// Test database connection
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+    console.error("Error connecting to database:", err.stack);
+    return;
+  }
+  console.log("Connected to database:", result.rows[0].now);
+});
+
+// running server on PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
