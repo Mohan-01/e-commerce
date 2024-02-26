b788894ab9387075a42931d71eec6a5925af1f5c
M. Mohan <20501a05c1@pvpsit.ac.in>
2024-02-26 09:21:27 +0530

    Added security layer and rate limit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+import { rateLimit } from 'express-rate-limit';
 
 import categoryRouter from './routes/categoryRoutes.js';
 import productRouter from './routes/productRoutes.js';
@@ -11,10 +15,30 @@ import cartItemRouter from './routes/cartItemRoutes.js';
 
 const app = express();
 
+const limiter = rateLimit({
+  max: 24,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests, please try again in an hour!'
+});
+
+// Set security HTTP Headers
+app.use(helmet());
+
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
 app.use(cookieParser());
 
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: []
+  })
+);
+app.use('/', limiter);
 // routes
 app.use('/categories', categoryRouter);
 app.use('/products', productRouter);
